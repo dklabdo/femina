a8295342efc3daecde628e3037320a7b561660fb
dklabdo <sayah.abdo22@gmail.com>
2024-06-30 04:11:12 +0100

    fix 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
-import Section1 from "./components/section1";
+import Section1 from "./components/Section1";
 import Section2 from "./components/Section2";
 import Section3 from "./components/Section3";
 import Mobile from "./components/Mobile";
