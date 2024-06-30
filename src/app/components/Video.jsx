adc10037d635915b75a703c5f1ce860ba8379225
dklabdo <sayah.abdo22@gmail.com>
2024-06-30 05:14:27 +0100

    change animation duration

--- src/app/components/Video.jsx
+++ src/app/components/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Video() {
   return (
     <div className='w-full py-28 px-2' >
-        <video  className='rounded-2xl  mx-auto' controls >
+        <video  className='rounded-2xl  mx-auto' autoPlay controls >
             <source src='/video.mp4' type='video/mp4' />
         </video>
 
